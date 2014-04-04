d11992dff0a84506bdcc7208013c37220eaefa9b
CaioIcy <caio.nardelli@hotmail.com>
2014-04-04 00:42:14 -0300

    Adding basic Log4J example

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -71,23 +71,43 @@ environments {
 
 // log4j configuration
 log4j = {
-    // Example of changing the log pattern for the default console appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+    appenders 
+    {
+        console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+        file name: 'applogtxt', file: 'target/drugstorelog.log',
+          layout: pattern(conversionPattern: "[%d{HH:mm:ss:SSS}] %-5p %c{2}: %m%n")
+    }
+
+    // Acting area for the appender
+    info additivity: false, applogtxt: 
+    [
+        'grails.app.controllers',
+        'grails.app.domain',
+        'grails.app.services',
+        'grails.app.taglib',
+        'grails.app.conf'
+    ]
+    
+    error stdout:
+    [
+        'org.codehaus.groovy.grails.web.servlet',        // controllers
+        'org.codehaus.groovy.grails.web.pages',          // GSP
+        'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+        'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+        'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+        'org.codehaus.groovy.grails.commons',            // core / classloading
+        'org.codehaus.groovy.grails.plugins',            // plugins
+        'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+        'org.springframework',
+        'org.hibernate',
+        'net.sf.ehcache.hibernate'
+    ]
+
+    root {
+       warn 'stdout', 'file'
+    }
+  
 }
 
 grails.config.defaults.locations = [KickstartResources]
--- grails-app/domain/entities/Client.groovy
+++ grails-app/domain/entities/Client.groovy
@@ -9,4 +9,37 @@ class Client extends Person {
     		email: true)
     }
 
+    // GORM Events examples.
+    def onLoad(){
+        log.info("Client loaded from database.")
+    }
+    
+    def beforeInsert(){
+         log.info("Client initially persisted onto the database.")
+    }
+
+    def beforeUpdate(){
+         log.info("Before Client update.")
+    }
+    
+    def beforeDelete(){
+         log.info("Before Client deletion.")
+    }
+    
+    def beforeValidate(){
+         log.info("Before Client validation.")
+    }
+
+    def afterInsert() {
+        log.info("After Client persisted onto the database.")
+    }
+
+    def afterUpdate(){
+         log.info("After Client update.")
+    }
+    
+    def afterDelete(){
+         log.info("After Client deletion.")
+    }
+
 }
