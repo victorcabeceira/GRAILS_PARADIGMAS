bb639e01a903c651f37a4bdde6e0ab9aa946b112
Leandro ROdrigues <leandrovelosorodrigues@gmail.com>
2014-04-03 23:23:17 -0300

    Arrumando configurações de banco

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,7 +48,7 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
+        runtime ":resources:1.2"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
@@ -58,7 +58,6 @@ grails.project.dependency.resolution = {
         build ":tomcat:$grailsVersion"
 
         runtime ":database-migration:1.3.2"
-
         compile ':cache:1.0.1'
         compile ":kickstart-with-bootstrap:0.9.6"
     }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,39 +1,37 @@
 dataSource {
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
-    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
-
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = true
+    cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
-
 // environment specific settings
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:mysql://127.0.0.1/paradigmas_grails?useUnicode=yes&characterEncoding=UTF-8"
-            username = "root"
-            password = "root"
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/paradigmas_grails?useUnicode=yes&characterEncoding=UTF-8"
+            username =  "root"
+            password = ""
         }
     }
     test {
         dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:mysql://127.0.0.1/paradigmas_grails?useUnicode=yes&characterEncoding=UTF-8"
-            username = "root"
-            password = "root"
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost/paradigmas_grails?useUnicode=yes&characterEncoding=UTF-8"
+            username =  "root"
+            password = ""
         }
     }
     production {
         dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:mysql://127.0.0.1/paradigmas_grails?useUnicode=yes&characterEncoding=UTF-8"
-            username = "root"
-            password = "root"
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost/paradigmas_grails?useUnicode=yes&characterEncoding=UTF-8"
+            username =  "root"
+            password = ""
+            
         }
     }
 }
